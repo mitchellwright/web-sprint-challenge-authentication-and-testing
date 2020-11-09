ace49e7d6ad65bded965df7f18c3c09fac3f603a
Mitchell Wright <mitchellbwright@gmail.com>
2020-11-08 20:27:19 -0600

    get authentication working

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,27 @@
-/* 
+/*
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
 
-module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+module.exports = async (req, res, next) => {
+  const { username, password } = req.body;
+  const user = await db("users").where({ username }).first();
+
+  if (!user) {
+    return res.status(401).json({
+      message: "Invalid username",
+    });
+  }
+
+  const passwordValid = await bcrypt.compare(password, user.password);
+
+  if (!passwordValid) {
+    res.status(401).json({
+      message: "Invalid password",
+    });
+  }
+
+  req.user = user;
+
+  next();
 };
